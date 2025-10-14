4653f0c549251dce589119d4c77592db7167cd12
Antoine Omond <antoine.omond@ntymail.com>
2025-10-14 21:06:19 +0200

    pdf without error

--- uit-inf-2200/assignment2/assign-repo.py
+++ uit-inf-2200/assignment2/assign-repo.py
@@ -5,33 +5,22 @@ import subprocess
 with open("name-email.csv") as f:
     name_email = [r for r in csv.reader(f)]
 abs_path = "/home/aomond/teaching_uit_2025/gradings/uit-inf-2200/assignment2/mips-simulator-submissions"
-#d = "/home/aomond/teaching_uit_2025/gradings/uit-inf-2200/assignment2/mips-simulator-submissions/mips-simulator-assignment-2"
 all_matches = {}
 for d in os.listdir(abs_path):
-    #print(d)
     student_dir = f"{abs_path}/{d}"
     matches = []
+    
+    # Check report
     try:
-        all_files = subprocess.check_output(f'find {student_dir} -type f | grep -Ev ".git|.png|.jpg|.jpeg|__pycache__|.DS_Store|.zip"', shell=True).decode("utf-8").strip().split("\n")
-        #all_files = subprocess.check_output(f'find {student_dir} -type f | grep .pdf', shell=True).decode("utf-8").strip().split("\n")
+        #all_files = subprocess.check_output(f'find {student_dir} -type f | grep -Ev ".git|.png|.jpg|.jpeg|__pycache__|.DS_Store|.zip"', shell=True).decode("utf-8").strip().split("\n")
+        all_files = subprocess.check_output(f'find {student_dir} -type f | grep .pdf', shell=True).decode("utf-8").strip().split("\n")
         for f_name in all_files:
-            if not f_name.endswith(".pdf"):
-                with open(f_name) as f:
-                    #print(f_name)
-                    try: 
-                        for l in f.read().split("\n"):
-                            for ne in name_email:
-                                for e in ne[:-1]:
-                                    if e in l:
-                                        m = f"{ne[0]},{ne[1]},{ne[-1]}"
-                                        if m not in matches:
-                                            matches.append(m)
-                    except UnicodeDecodeError as e:
-                        print(f_name)
-                        print(e)
-            else:
+            try:
+                is_pdf = subprocess.check_output(f'file -b "{f_name}" | grep -o "PDF"', shell=True).decode("utf-8").strip() == "PDF"
+            except subprocess.CalledProcessError as e:
+                is_pdf = False
+            if is_pdf:
                 for ne in name_email:
-                    #search_string = "|".join(ne[:-1])
                     search_string = "|".join(ne[:-1])
                     try:
                         g = subprocess.check_output(f'pdfgrep -Pi --cache "{search_string}" "{f_name}"', shell=True)
@@ -40,19 +29,25 @@ for d in os.listdir(abs_path):
                             matches.append(m)
                     except subprocess.CalledProcessError as e:
                         continue
-                    #matches.append((e, ne, f_name))
     except subprocess.CalledProcessError:
         pass
-    #for k in matches:
-    #    print(k)
+        
+    # Check commit logs
+    r = subprocess.check_output(f"git -C {student_dir} log --pretty=format:'%an %ae'", shell=True).decode("utf-8").strip()
+    for l in r.split("\n"):
+        *n,e = l.split()
+        for ne in name_email:
+            if " ".join(n) in ne or e in ne:
+                m = f"{ne[0]},{ne[1]},{ne[-1]}"
+                if m not in matches:
+                    matches.append(m)
     
+    # Check if empty repo
     if matches == []:
         r = subprocess.check_output(f"git -C {student_dir} log -1 --pretty=format:'%an %s'", shell=True).decode("utf-8").strip()
-        #print(r, r == "github-classroom[bot] Initial commit")
         if r == "github-classroom[bot] Initial commit":
             matches.append("Empty repository")
     all_matches[d] = matches
-    #print(all_files)
 max_size = len(max((k for k in all_matches.keys()), key=len))
 for k,v in all_matches.items():
     print(f"{k} {' '*(max_size-len(k))} | {v}")
