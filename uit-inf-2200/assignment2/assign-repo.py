21c823538d3a465dbcdffb0c8ae810ffc48ae418
Antoine Omond <antoine.omond@ntymail.com>
2025-10-14 20:18:03 +0200

    identify empty repos

--- uit-inf-2200/assignment2/assign-repo.py
+++ uit-inf-2200/assignment2/assign-repo.py
@@ -23,7 +23,9 @@ for d in os.listdir(abs_path):
                             for ne in name_email:
                                 for e in ne[:-1]:
                                     if e in l:
-                                        matches.append((ne, f_name))
+                                        m = f"{ne[0]},{ne[1]},{ne[-1]}"
+                                        if m not in matches:
+                                            matches.append(m)
                     except UnicodeDecodeError as e:
                         print(f_name)
                         print(e)
@@ -32,7 +34,7 @@ for d in os.listdir(abs_path):
                     #search_string = "|".join(ne[:-1])
                     search_string = "|".join(ne[:-1])
                     try:
-                        g = subprocess.check_output(f'pdfgrep -Pi "{search_string}" "{f_name}"', shell=True)
+                        g = subprocess.check_output(f'pdfgrep -Pi --cache "{search_string}" "{f_name}"', shell=True)
                         m = f"{ne[0]},{ne[1]},{ne[-1]}"
                         if m not in matches:
                             matches.append(m)
@@ -43,9 +45,14 @@ for d in os.listdir(abs_path):
         pass
     #for k in matches:
     #    print(k)
+    
+    if matches == []:
+        r = subprocess.check_output(f"git -C {student_dir} log -1 --pretty=format:'%an %s'", shell=True).decode("utf-8").strip()
+        #print(r, r == "github-classroom[bot] Initial commit")
+        if r == "github-classroom[bot] Initial commit":
+            matches.append("Empty repository")
     all_matches[d] = matches
     #print(all_files)
 max_size = len(max((k for k in all_matches.keys()), key=len))
 for k,v in all_matches.items():
     print(f"{k} {' '*(max_size-len(k))} | {v}")
-
